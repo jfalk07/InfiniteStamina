acce72af8991f72df9058ee255e8de1fb7fe9a52
Minemuffin <jojo071220@gmail.com>
2025-05-11 01:34:39 +0200

    Initial

---
+++ TestMod1/Patch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace InfiniteStamina;
+
+public class InfiniteStaminaPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(WildSkies.Player.LocalPlayer), "Awake")]
+    static bool InfiniteStamina(WildSkies.Player.LocalPlayer __instance)
+    {
+        __instance._staminaController._infiniteStaminaEnabled = true;
+        return true;
+    }
+}
---
+++ TestMod1/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace InfiniteStamina;
+
+[BepInPlugin("org.minemuffin.plugins.InfiniteStamina", "Infinite Stamina", "0.0.1")]
+public class InfiniteStaminaPlugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        Harmony harmony = new Harmony("minemuffin.InfiniteStamina");
+        harmony.PatchAll(typeof(InfiniteStaminaPatch));
+        
+        Log = base.Log;
+        Log.LogInfo($"Plugin org.minemuffin.plugins.InfiniteStamina is loaded!");
+    }
+}
